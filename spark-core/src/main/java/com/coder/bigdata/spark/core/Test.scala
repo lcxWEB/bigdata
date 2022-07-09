5c033c4e58648184de8122274415bdb8982eb900
cx.li <chunxialims@163.com>
2022-07-09 12:37:01 +0800

    init project

---
+++ spark-core/src/main/java/com/coder/bigdata/spark/core/Test.scala
@@ -0,0 +1,13 @@
+package com.coder.bigdata.spark.core
+
+/**
+  * @author: lichunxia 
+  * @create: 7/9/22 12:29 PM
+  */
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    println("hello Spark");
+  }
+
+}
